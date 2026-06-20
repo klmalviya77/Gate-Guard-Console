53e9c24a11ae0ea3865f544d4b6f13727265427d
klmalviya77 <klmalviya77@gmail.com>
2026-06-20 18:29:00 +0530

    pwa

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "gateguard-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+// Install Event - Caching files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch Event - Serving from cache if offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate Event - Clearing old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
